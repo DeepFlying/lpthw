b913222812f50fa8eb0a0e4d0a10bfc6d6f62346
sawyer <33996991+DeepFlying@users.noreply.github.com>
2018-11-19 16:05:15 +0800

    Add files via upload

---
+++ ex5.py
@@ -0,0 +1,18 @@
+my_name = 'Li Ya'
+my_age = 23
+my_height = 178
+my_weight = 60
+my_eyes = 'Black'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height } cm tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Acually that's not too light.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on the food.")
+
+#this line is tricky,try to get it exactly right
+total = my_age + my_height + my_weight
+print(f"If I add {my_age}, {my_height}, and {my_weight} I get {total}.")
---
+++ ex6.py
@@ -0,0 +1,43 @@
+# the number of  type of people
+types_of_people = 10
+
+# x is f-string 
+x = f"There are {types_of_people} types of people."
+
+#binary is a string variable
+binary = "binary"
+
+#do_not is a string variable
+do_not = "don't"
+
+# y is a string variable
+y = f"Those who know {binary} and those who {do_not}."
+
+#print string x
+print(x)
+
+#print string y
+print(y)
+
+#print a combined string with x
+print(f"I said: {x}")
+
+#print a combined string with y
+print(f"I also said: '{y}'")
+
+#not known
+hilarious = "False"
+#not known
+joke_evaluation = "Isn't that joke so funny?! {}"
+
+#print a format string
+print(joke_evaluation.format(hilarious))
+
+# w is a string variable
+w = "This is the left side of ..."
+
+#e is a string variable
+e =  "a string with a right side."
+
+#print a string which w and e are combined
+print(w + e)
