133d24087711d12065fedaade4b9e45eaed5c005
sawyer <sawyer@hust.edu.cn>
2018-11-21 09:29:37 +0800

    add the comment

--- ex15.py
+++ ex15.py
@@ -1,15 +1,26 @@
+#use argv to get user input
 from sys import argv
 
+#unpack the input
 script, filename = argv
 
+#open the file
 txt = open(filename)
 
+#print the filename
 print(f"Here's your file {filename}:")
+
+#print the file's content
 print(txt.read())
 
+#input a new file
 print("Type the filename again:")
+
+#input the file after >
 file_again = input("> ")
 
+#open the file
 txt_again = open(file_again)
 
+#print the file's content
 print(txt_again.read())
