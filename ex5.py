9baa2582398ad4ca3615e000509826e0d4fdb3a6
sawyer <sawyer@hust.edu.cn>
2018-11-28 15:56:24 +0800

    add a title

--- ex5.py
+++ ex5.py
@@ -1,14 +1,15 @@
-my_name = 'Li Ya'
+#More Variable and Printing
+my_name = 'Tom Sawyer'
 my_age = 23
 my_height = 178
 my_weight = 60
 my_eyes = 'Black'
 my_teeth = 'White'
 my_hair = 'Black'
-
+#f represents format printing 
 print(f"Let's talk about {my_name}.")
-print(f"He's {my_height } cm tall.")
-print(f"He's {my_weight} kg heavy.")
+print(f"He's {my_height }cm tall.")
+print(f"He's {my_weight}kg heavy.")
 print("Acually that's not too light.")
 print(f"He's got {my_eyes} eyes and {my_hair} hair.")
 print(f"His teeth are usually {my_teeth} depending on the food.")
