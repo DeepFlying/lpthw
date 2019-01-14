9104e81830838bcd4bb52b544c639d9f781fcd14
sawyer <sawyer@hust.edu.cn>
2019-01-14 16:29:41 +0800

    modified the file

--- ex40a.py
+++ ex40a.py
@@ -1,6 +1,22 @@
 #Modules, Classes, Objects
-# mystuff = {'apple': "I AM APPLES!"}
-# print(mystuff['apple'])
+#method1
+mystuff = {'apple': "I AM APPLES!"}
+print(mystuff['apple'])
+
+#method2
 import mystuff
 mystuff.apple()
 print(mystuff.tangerine)
+
+#method3
+class MyStuff(object):
+
+    def __init__(self):
+        self.tangerine = "And now a thousand years between"
+
+    def apple(self):
+        print("I AM CLASSY APPLES!")
+
+thing = MyStuff()
+thing.apple()
+print(thing.tangerine)
